cd35febf404d0f56da6265fad4e1854f1a476bb4
NUG01 <nugoskhireli@redberry.ge>
2022-12-23 19:49:44 +0400

    fix: localization

--- src/components/forms/PasswordforgetForm.vue
+++ src/components/forms/PasswordforgetForm.vue
@@ -41,16 +41,16 @@ export default {
 
 <template>
    <Form @submit="onSubmit" class="w-[36rem] sm:w-[32rem]">
-      <basic-input rules='required|email|email_valid' name="email" type="email" placeholder="Enter your email" label="Email"/>      
+      <basic-input rules='required|email|email_valid' name="email" type="email" :placeholder="$t('landing.email_placeholder')" :label="$t('landing.email')"/>      
       <div id="div" v-if="errors.length>0">
       <div @click="deleteError" class="cursor-pointer bg-[#dcb9bb] text-[#222030] text-[1.6rem] px-[7px] py-[4px] rounded-[4px]"><div class="flex items-center justify-between"><p>{{ errors[0] }}</p><invalid-icon/></div></div>
       </div>
       <div class="flex mb-[3.2rem] mt-[2.4rem] justify-between">
-      <basic-button type="submit" class="text-[white] text-[1.6rem] border border-solid bg-[#E31221] border-[#E31221] px-[25.5px] py-[7px] rounded-[4px]" width="w-[100%]">Send instructions</basic-button>
+      <basic-button type="submit" class="text-[white] text-[1.6rem] border border-solid bg-[#E31221] border-[#E31221] px-[25.5px] py-[7px] rounded-[4px]" width="w-[100%]">{{ $t('landing.send_instructions') }}</basic-button>
       </div>
       <router-link :to="{name:'login'}"><div class="flex items-center justify-center gap-[1rem]">
         <back-arrow></back-arrow>
-        <button type="button" class="text-[#6C757D] text-[1.6rem]">Back to log in</button>
+        <button type="button" class="text-[#6C757D] text-[1.6rem]">{{ $t('landing.back_login') }}</button>
       </div>
       </router-link>
       </Form>
--- src/views/PasswordRecoverView.vue
+++ src/views/PasswordRecoverView.vue
@@ -46,10 +46,10 @@ export default {
     <div class="fixed top-0 left-0 w-[100vw] h-[100vh] backdrop-blur-[3px] bg-[rgba(0,0,0,0.54)] z-50" @click="modalHide"></div>
     <div class="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col items-center justify-center px-[12rem] py-[5.4rem] bg-[#222030] rounded-[10px] z-50">
        <form-header>
-        Create new password
+        {{ $t('landing.create_password') }}
         <template v-slot:secondaryText>
-         Your new password must be different from<br>
-previous used passwords
+         {{ $t('landing.different_new_password') }}<br>
+{{ $t('landing.previous_password') }}
         </template>
       </form-header>
      <passwordrecover-form @is-recovered="changeRecoverStatus"></passwordrecover-form>
@@ -60,10 +60,10 @@ previous used passwords
     <div class="max-w-[54rem] md:max-w-[100vw] md:min-w-[100vw] fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col items-center justify-center px-[8rem] pt-[5rem] pb-[6rem] md:px-[3rem] md:pt-[2rem] md:pb-[2.4rem] bg-[#222030] rounded-[10px] z-50">
       <verified-tick class="mb-[2rem] mt-[1.4rem]"></verified-tick>
       <div class="flex flex-col text-center gap-[2rem] mb-[3rem]">
-      <p class="text-[3.2rem] text-[#ffffff] font-medium">Success!</p>
-      <p class="text-[1.6rem] text-[#ffffff]">Your Email changed successfully</p>
+      <p class="text-[3.2rem] text-[#ffffff] font-medium">{{ $t('landing.success') }}</p>
+      <p class="text-[1.6rem] text-[#ffffff]">{{ $t('landing.email_changed') }}</p>
       </div>
-      <router-link :to="{name:'login'}" style="width:100%"><basic-button type="button" class="text-[white] text-[1.6rem] border border-solid bg-[#E31221] border-[#E31221] px-[7rem] py-[7px] rounded-[4px]" width="w-[100%]">Go to my news feed</basic-button></router-link>
+      <router-link :to="{name:'login'}" style="width:100%"><basic-button type="button" class="text-[white] text-[1.6rem] border border-solid bg-[#E31221] border-[#E31221] px-[7rem] py-[7px] rounded-[4px]" width="w-[100%]">{{ $t('landing.go_to_feed') }}</basic-button></router-link>
 </div>
   </div>
 </div>
