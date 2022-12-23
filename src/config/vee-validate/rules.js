6b4fb7750d0e3ed5172554745b44986c99b968dc
NUG01 <nugoskhireli@redberry.ge>
2022-12-23 14:40:38 +0400

    fix: validation and mobile search bugs

--- src/config/vee-validate/rules.js
+++ src/config/vee-validate/rules.js
@@ -17,10 +17,10 @@ defineRule('lower_case', value => {
   const locale=ref(i18n.global.locale)
  
 if (value && !(regexDual.test(value))) {
-  if(locale=='en'){
+  if(locale.value=='en'){
     return 'The password field must consist of lower case and latin characters';
     
-  }else{
+  }else if(locale.value=='ka'){
     return 'პაროლი უნდა იყოს დაბალი რეგისტრის და ლათინურად';
 
   }
@@ -29,11 +29,12 @@ if (value && !(regexDual.test(value))) {
 }
  });
 defineRule('lower_case_name', value => {
+  const locale=ref(i18n.global.locale)
 if (value && !(regexDual.test(value))) {
-  if(locale=='en'){
+  if(locale.value=='en'){
     return 'The name field must consist of lower case and latin characters';
     
-  }else{
+  }else if(locale.value=='ka'){
     return 'სახელი უნდა იყოს დაბალი რეგისტრის და ლათინურად';
 
   }
@@ -45,10 +46,11 @@ if (value && !(regexDual.test(value))) {
 const regexSpecialCharacters=/[-._!"`'#%&,:;<>=@{}~\$\(\)\*\+\/\\\?\[\]\^\|]+/;
 const regexEnglish=/^[A-Za-z0-9 ]*$/;
 defineRule('eng_alphabet', value => {
+  const locale=ref(i18n.global.locale)
   if (value && !regexEnglish.test(value) && !regexSpecialCharacters.test(value)) {
-    if(locale=='en'){
+    if(locale.value=='en'){
       return 'Please use latin symbols and numbers';      
-    }else{
+    }else if(locale.value=='ka'){
       return 'გამოიყენეთ ლათინური სიმბოლოები';
   
     }
@@ -59,10 +61,11 @@ defineRule('eng_alphabet', value => {
 
 const regexGeorgian=/^[ა-ჰ-0-9 ]*$/;
 defineRule('geo_alphabet', value => {
+  const locale=ref(i18n.global.locale)
 if (value && !regexGeorgian.test(value) && !regexSpecialCharacters.test(value)) {
-  if(locale=='en'){
+  if(locale.value=='en'){
     return 'Please use Georgian symbols and numbers';
-  }else{
+  }else if(locale.value=='ka'){
     return 'გამოიყენეთ ქართული სიმბოლოები და რიცხვები';
 
   }
@@ -73,10 +76,11 @@ if (value && !regexGeorgian.test(value) && !regexSpecialCharacters.test(value)) 
 
 const regexEmail=/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 defineRule('email_valid', value => {
+  const locale=ref(i18n.global.locale)
 if (value && !regexEmail.test(value)) {
-  if(locale=='en'){
+  if(locale.value=='en'){
     return 'This email must be in a valid format';
-  }else{
+  }else if(locale.value=='ka'){
     return 'ელ.ფოსტა უნდა იყოს ვალიდურ ფორმატში';
   }
   }else{
